afec7beba8b65be60616a0e204e0f2c40e100235
Romy <carito@gmail.com>
2012-02-19 01:32:17 -0500

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views # we manually insert this line to render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title, yo" do
+      get 'home'
+      response.should have_selector("title", # have_selector method in Rspec! :)
+        :content => "Look, I said aliens are invading our planet... | ")
     end
   end
 
@@ -14,6 +21,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right contact title, yo" do
+      get "contact" 
+      response.should have_selector("title", 
+        :content => "Look, I said aliens are invading our planet... | ")
+    end
   end
 
+  describe "get 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the proper about title, yo" do
+      get "about" 
+      response.should have_selector("title",
+        :content => "Look, I said aliens are invading our planet... | ")
+    end
+  end
 end
